5dcf0805abb83148c48a32071e2fd4eae7d6d047
sven <sven@so36.net>
2013-12-16 16:25:37 +0100

    Install PLone dependencies on ubuntu

---
+++ ubuntu/scripts/plone.sh
@@ -0,0 +1,4 @@
+# INstall Plone Dependencies
+apt-get install build-essential libssl-dev libxml2-dev libxslt1-dev libbz2-dev \
+    zlib1g-dev python-setuptools python-dev libjpeg62-dev \
+    libreadline-gplv2-dev python-imaging wv poppler-utils
