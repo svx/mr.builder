ca43be6ae3946af0c1fcd77a866aa69d251a75e6
sven <sven@so36.net>
2014-01-14 18:05:16 +0100

    Update: base.sh

--- debian/scripts/base.sh
+++ debian/scripts/base.sh
@@ -5,7 +5,7 @@ apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
 apt-get -y install curl unzip
 
 # Set up sudo
-echo 'svx ALL=NOPASSWD:ALL' > /etc/sudoers.d/svx
+#echo 'svx ALL=NOPASSWD:ALL' > /etc/sudoers.d/svx
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
