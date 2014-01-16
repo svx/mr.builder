03b4b044c333c09a48ad8c142ec6d6104c7d18fd
sven <sven@so36.net>
2014-01-16 14:35:04 +0100

    Add grub timeout boost

--- ubuntu/scripts/base.sh
+++ ubuntu/scripts/base.sh
@@ -6,3 +6,18 @@ apt-get -y install linux-headers-$(uname -r)
 #sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
+
+# Remove 5s grub timeout to speed up booting
+cat <<EOF > /etc/default/grub
+# If you change this file, run 'update-grub' afterwards to update
+# /boot/grub/grub.cfg.
+
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX="debian-installer=en_US"
+EOF
+
+update-grub
+
