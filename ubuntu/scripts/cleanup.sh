f554d5701af8b0baf4bdac852a98b4be5bb6960e
sven <sven@so36.net>
2014-01-16 14:00:19 +0100

    Wording

--- ubuntu/scripts/cleanup.sh
+++ ubuntu/scripts/cleanup.sh
@@ -1,16 +1,16 @@
 apt-get -y autoremove
 apt-get -y clean
 
-echo "cleaning up gest additions"
+echo "Cleaning up gest additions"
 rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
 
-echo "cleaning up dhcp leases"
+echo "Cleaning up dhcp leases"
 if [ -d "/var/lib/dhcp" ]; then
     # Remove leftover leases and persistent rules
     rm /var/lib/dhcp/*
 fi
 
-echo "cleaning up udev rules"
+echo "Cleaning up udev rules"
 rm /etc/udev/rules.d/70-persistent-net.rules
 mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
@@ -22,7 +22,7 @@ echo "pre-up sleep 2" >> /etc/network/interfaces
 echo "Cleaning /tmp"
 rm -rf /tmp/*
 
-echo "cleaning logs"
+echo "Cleaning logs"
 find /var/log -type f -exec /bin/cp /dev/null {} \;
 
 
