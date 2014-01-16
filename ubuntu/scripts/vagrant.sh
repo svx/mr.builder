bdfa64d4907c79c219540aee9dc428f0f71c6c31
sven <sven@so36.net>
2014-01-16 14:03:02 +0100

    Remove old config

--- ubuntu/scripts/vagrant.sh
+++ ubuntu/scripts/vagrant.sh
@@ -25,9 +25,3 @@ wget --no-check-certificate "${VAGRANT_KEY_URL}" -O authorized_keys
 chmod 600 $VAGRANT_HOME/.ssh/authorized_keys
 chown -R $VAGRANT_USER:$VAGRANT_USER $VAGRANT_HOME/.ssh
 
-#mkdir /home/vagrant/.ssh
-#wget --no-check-certificate \
-#    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
-#    -O /home/vagrant/.ssh/authorized_keys
-#chown -R vagrant /home/vagrant/.ssh
-#chmod -R go-rwsx /home/vagrant/.ssh
