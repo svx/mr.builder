7eee8fd8148120c498371da78f374c45a23aac34
sven <sven@so36.net>
2014-01-20 12:27:33 +0100

    More updates on plone.sh

--- ubuntu/scripts/plone.sh
+++ ubuntu/scripts/plone.sh
@@ -5,4 +5,12 @@ apt-get install build-essential libssl-dev libxml2-dev libxslt1-dev libbz2-dev \
     zlib1g-dev python-setuptools python-dev libjpeg62-dev \
     libreadline-gplv2-dev python-imaging wv poppler-utils
 
+echo "Downloading Plone"
+wget https://launchpad.net/plone/4.3/4.3.2/+download/Plone-4.3.2-UnifiedInstaller.tgz
 
+echo "Extracting download"
+tar xf Plone-4.3.2-UnifiedInstaller.tgz
+
+echo "Building Plone, this can take some minutes ..."
+cd Plone-4.3.2-UnifiedInstaller/
+./install.sh --password=plone zeo
