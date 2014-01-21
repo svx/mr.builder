12d683df02e961701616a827478734b23e27d597
sven <sven@so36.net>
2014-01-21 15:32:38 +0100

    Add centos

--- builder.sh
+++ builder.sh
@@ -7,6 +7,7 @@ echo "List of Options:"
 echo
 echo "[1]  Debian 7 Virtualbox Image"
 echo "[2]  Ubuntu 12.04 Virtualbox Image"
+echo "[3]  Centos 6.5 Virtualbox Image"
 echo
 
 read os
@@ -15,12 +16,15 @@ case "$os" in
 # Note variable is quoted.
 
 "1" | "d" )
-echo "Will build debian"
+echo "Will build Debian"
 ;;
 
 "2" | "u" )
-echo "Will build ubuntu"
+echo "Will build Ubuntu"
 ;;
+
+"3" )
+echo "Will build Centos"
 
 esac
 
