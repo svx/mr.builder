c4ff6bfb26b42d4be74b78e5c1530f94375ef026
sven <sven@so36.net>
2014-02-12 10:05:16 +0100

    Add python2.6 and pillow for 2.6

--- ubuntu/scripts/pjc.sh
+++ ubuntu/scripts/pjc.sh
@@ -44,7 +44,7 @@ apt-get install -y python-tk
 apt-get install -y openjdk-7-jre
 
 # LXML
-apt-get install -y libxslt1-dev libxml2-dev
+apt-get install -y libxslt1-dev libxml2-dev python-lxml
 
 # LOCALES
 echo "en_GB.ISO8859-15 ISO-8859-15" >> /var/lib/locales/supported.d/local
@@ -52,4 +52,32 @@ echo "en_US.ISO8859-15 ISO-8859-15" >> /var/lib/locales/supported.d/local
 echo "en_US.ISO-8859-1 ISO-8859-1" >> /var/lib/locales/supported.d/local
 dpkg-reconfigure locales
 
+# Install python2.7
+apt-get install -y python2.7
+apt-get install -y python2.7-dev
 
+# Install python2.6
+add-apt-repository -y ppa:fkrull/deadsnakes
+apt-get update
+apt-get install -y python2.6 python2.6-dev
+
+# Install setuptools for python2.6
+wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python2.6
+
+# Install pip for python2.6
+python2.6 -m easy_install pip
+
+# Install pillow for python2.6
+pip2.6 install pillow
+
+# Test Coverage
+apt-get install -y enscript
+
+# Install Setuptools
+wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python2.7
+
+# Install xfvb
+apt-get install -y xvfb
+apt-get install -y x11-apps
+apt-get install -y xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
+apt-get install -y imagemagick
