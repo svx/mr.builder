aa030c902951db812e2c0430855833d21c58ef8b
sven <sven@so36.net>
2014-02-12 16:40:05 +0100

    Add python2.7-dev

--- ubuntu/scripts/plone.sh
+++ ubuntu/scripts/plone.sh
@@ -5,7 +5,7 @@ PlONE_DOWNLOAD=https://launchpad.net/plone/4.3/4.3.2/+download/Plone-4.3.2-Unifi
 
 echo "Installing needed packages for plone"
 apt-get install -y build-essential libssl-dev libxml2-dev libxslt1-dev libbz2-dev \
-    zlib1g-dev python-setuptools python-dev libjpeg62-dev \
+    zlib1g-dev python-setuptools python-dev libjpeg62-dev python2.7-dev \
     libreadline-gplv2-dev python-imaging wv poppler-utils supervisor
 
 echo "Downloading Plone"
