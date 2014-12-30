cfb890ea5c7c6e7f7e2fd0c237ec8d0447b899ba
sven <sven@so36.net>
2014-12-30 09:37:19 +0100

    Tweak docker install, add dns settings

--- debian/scripts/docker.sh
+++ debian/scripts/docker.sh
@@ -3,4 +3,5 @@ echo 'deb http://http.debian.net/debian wheezy-backports main' >> /etc/apt/sourc
 apt-get update -y
 apt-get install -y linux-image-amd64
 curl -sSL https://get.docker.com/ | sh
-
+gapsswd -a vagrant docker
+echo 'DOCKER_OPTS="--dns 8.8.8.8"' >> /etc/default/docker
